d234dcd72278dfa64dd07174a66736ac69cc023c
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2017-05-01 22:48:31 +0300

    Somewhat More Usable

--- src/everything_except/core.clj
+++ src/everything_except/core.clj
@@ -6,6 +6,8 @@
 (def keycode-prefix-length (count keycode-prefix))
 (def line-width 7)
 (def separator "|")
+(def cell-seperator "+")
+(def modifier-function-regex #"[A-Z]+\([A-Z]+_([A-Z]+)\)")
 
 (defn- get-last-letter [a-string]
   (let [length (count a-string)]
@@ -24,11 +26,18 @@
 (defn- get-keycodeless-suffix [key]
   (subs key keycode-prefix-length))
 
+(defn- get-special-key [key]
+  (let [modifier-function (re-find modifier-function-regex key)]
+    (if (nil? modifier-function)
+      (get-keycodeless-suffix key)
+      (last modifier-function))))
+
 (defn- get-short-form [key]
   (if (s/starts-with? key keycode-prefix)
     (if (is-simple-key? key)
       (get-last-letter key)
-      (get-keycodeless-suffix key))))
+      key)
+    (get-special-key key)))
 
 (defn- divide-and-round-up [dividend divisor]
   (inc (Math/floorDiv dividend divisor)))
@@ -42,20 +51,41 @@
       (print char)
       (print-char-x-times char (dec x)))))
 
+(defn- get-margins [empty-cols]
+  (let [right-margin (divide-and-round-down empty-cols 2)
+        increment (if (odd? empty-cols) 1 0)]
+    {:left (+ increment right-margin)
+     :right right-margin}))
+
 (defn- draw-key [key]
   (let [blank-cols (- line-width (count key))
-        left-margin (divide-and-round-down blank-cols 2)
-        right-margin (divide-and-round-up blank-cols 2)]
+        margin (get-margins blank-cols)]
     (print separator)
-    (print-char-x-times " " left-margin)
+    (print-char-x-times " " (:left margin))
     (print key)
-    (print-char-x-times " " right-margin)))
+    (print-char-x-times " " (:right margin))))
+
+(defn- repeat-char [a-char n]
+  (apply str (repeat n a-char)))
+
+(defn- print-cell-base []
+  (print cell-seperator)
+  (print (repeat-char "-" line-width)))
+
+(defn- print-base-line [num-cells]
+  (if (= 0 num-cells)
+    (println)
+    (do
+      (print-cell-base)
+      (print-base-line (dec num-cells)))))
 
 (defn- draw-line [keys]
   (let [short-form-keys (map get-short-form keys)]
     (loop [ks short-form-keys]
       (if (empty? ks)
-        (println)
+        (do
+          (println)
+          (print-base-line (count short-form-keys)))
         (do
           (draw-key (first ks))
           (recur (rest ks)))))))
