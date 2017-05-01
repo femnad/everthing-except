e658d34fa55a883edcecccef50f7d50c9f7f3c0e
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2017-05-01 22:48:43 +0300

    Defer Printing Until Ready

--- src/everything_except/core.clj
+++ src/everything_except/core.clj
@@ -5,7 +5,7 @@
 (def keycode-prefix "KC_")
 (def keycode-prefix-length (count keycode-prefix))
 (def line-width 7)
-(def separator "|")
+(def vertical-separator "|")
 (def cell-seperator "+")
 (def modifier-function-regex #"[A-Z]+\([A-Z]+_([A-Z]+)\)")
 
@@ -20,24 +20,19 @@
      (map s/trim)
      (filter not-empty))))
 
-(defn- is-simple-key? [key]
-  (= (count key) (inc keycode-prefix-length)))
-
-(defn- get-keycodeless-suffix [key]
+(defn- get-suffix [key]
   (subs key keycode-prefix-length))
 
-(defn- get-special-key [key]
+(defn- parse-modifier-function [key]
   (let [modifier-function (re-find modifier-function-regex key)]
     (if (nil? modifier-function)
-      (get-keycodeless-suffix key)
+      key
       (last modifier-function))))
 
 (defn- get-short-form [key]
   (if (s/starts-with? key keycode-prefix)
-    (if (is-simple-key? key)
-      (get-last-letter key)
-      key)
-    (get-special-key key)))
+    (get-suffix key)
+    (parse-modifier-function key)))
 
 (defn- divide-and-round-up [dividend divisor]
   (inc (Math/floorDiv dividend divisor)))
@@ -45,28 +40,21 @@
 (defn- divide-and-round-down [dividend divisor]
   (Math/floorDiv dividend divisor))
 
-(defn- print-char-x-times [char x]
-  (when-not (= 0 x)
-    (do
-      (print char)
-      (print-char-x-times char (dec x)))))
-
 (defn- get-margins [empty-cols]
   (let [right-margin (divide-and-round-down empty-cols 2)
         increment (if (odd? empty-cols) 1 0)]
     {:left (+ increment right-margin)
      :right right-margin}))
 
-(defn- draw-key [key]
-  (let [blank-cols (- line-width (count key))
-        margin (get-margins blank-cols)]
-    (print separator)
-    (print-char-x-times " " (:left margin))
-    (print key)
-    (print-char-x-times " " (:right margin))))
-
 (defn- repeat-char [a-char n]
   (apply str (repeat n a-char)))
+
+(defn- get-key [key]
+  (let [blank-cols (- line-width (count key))
+        margin (get-margins blank-cols)]
+    (apply str (repeat-char " " (:left margin))
+          key
+          (repeat-char " " (:right margin)))))
 
 (defn- print-cell-base []
   (print cell-seperator)
@@ -79,19 +67,19 @@
       (print-cell-base)
       (print-base-line (dec num-cells)))))
 
-(defn- draw-line [keys]
+(defn- get-line [keys]
   (let [short-form-keys (map get-short-form keys)]
-    (loop [ks short-form-keys]
+    (loop [ks short-form-keys line-acc []]
       (if (empty? ks)
-        (do
-          (println)
-          (print-base-line (count short-form-keys)))
-        (do
-          (draw-key (first ks))
-          (recur (rest ks)))))))
+        line-acc
+        (let [head (first ks)
+              tail (rest ks)]
+          (recur tail
+                 (conj line-acc
+                       (get-key head))))))))
 
 (defn generate-olkb-keymap-map [keymap]
   (let [lines (s/split keymap #"\n")]
     (->> lines
         (map get-keys-from-line)
-        (map draw-line))))
+        (map get-line))))
